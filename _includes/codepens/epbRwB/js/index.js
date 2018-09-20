2f730b894588b17f3c0a7095fbc6b3d2920b0173
Dylan Just <41112113+tinydylan@users.noreply.github.com>
2018-09-20 23:48:13 +1000

    Codepens

--- _includes/codepens/epbRwB/js/index.js
+++ _includes/codepens/epbRwB/js/index.js
@@ -6,8 +6,7 @@ tinymce.init({
         "searchreplace visualblocks code fullscreen",
         "insertdatetime media table contextmenu paste imagetools wordcount"
     ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-  // imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+  toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
     '//www.tinymce.com/css/codepen.min.css'
